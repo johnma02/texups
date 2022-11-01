00f7de603f0ded7fc85bd0efb233ae7b71a10e0c
johnma <ma.jonathan02@gmail.com>
2022-10-31 23:47:46 -0400

    did some research stuff

---
+++ 220/hw4/bst/dsexceptions.h
@@ -0,0 +1,12 @@
+// Mark Weiss, Data Structures and Algorithm Analysis in C++, 4th edition
+#ifndef DS_EXCEPTIONS_H
+#define DS_EXCEPTIONS_H
+
+class UnderflowException { };
+class IllegalArgumentException { };
+class ArrayIndexOutOfBoundsException { };
+class IteratorOutOfBoundsException { };
+class IteratorMismatchException { };
+class IteratorUninitializedException { };
+
+#endif
